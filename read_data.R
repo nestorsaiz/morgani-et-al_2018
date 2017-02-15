af84d555ae69b38a7840d1fdc84847d917902f23
nestorsaiz <nestorsaiz@icloud.com>
2017-02-15 17:55:37 -0500

    minor updates, new embryos

--- read_data.R
+++ read_data.R
@@ -79,6 +79,9 @@ spry$Treatment <- factor(spry$Treatment, levels = c('Littermate', 'Control',
 spry$CH2.marker <- factor(spry$CH2.marker, levels = c('Venus', 'GFP.ck'))
 spry$ab.2 <- factor(spry$ab.2, levels = c('NA', 'Hoechst', 'af488.ck', 
                                           'af568.gt', 'af647.rb'))
+spry$Treatment <- factor(spry$Treatment, levels = c('Littermate', 'Control', 
+                                                    'FGF4_1000', 'AZD_1', 
+                                                    'PD03_1', 'neg.control'))
 
 ## Calculate number of embryos for each group (stage x treatment x genotype x IF)
 n.embryos <- spry %>% group_by(Embryo_ID, Stage, Treatment, 
@@ -91,3 +94,54 @@ n.embryos2 <- dcast(subset(n.embryos, Treatment != 'neg.control'),
                    Genotype + Stage ~ interaction(Treatment, CH2.marker), 
                    value.var = 'N')
 write.csv(n.embryos2, file = 'N_numbers.csv', row.names = FALSE)
+
+## Calculate the average level for each fluorescence channel
+## for each embryo and lineage
+meantensity <- spry %>%
+        group_by(Experiment, Litter, Embryo_ID, Identity.lin, TE_ICM, 
+                 Cellcount, Stage, Exp_date, Img_date, Genotype, 
+                 Treatment, Tt_length, Tt_stage, CH2.marker, ab.2) %>%
+        summarize(Count = n(), 
+                  CH2.mean = mean(CH2.ebLogCor), 
+                  CH3.mean = mean(CH3.ebLogCor), 
+                  CH5.mean = mean(CH5.ebLogCor))
+
+## Calculate the number of cells per lineage for each embryo
+spry.lincounts <- spry %>% filter(Treatment == 'Littermate') %>% 
+        group_by(Experiment, Litter, 
+                 Embryo_ID, TE_ICM, 
+                 Cellcount, Stage, 
+                 Exp_date, Img_date, 
+                 Genotype, Treatment, 
+                 Identity.lin, CH2.marker, ab.2) %>%
+        summarize(count = n())
+
+## Calculate the ratio of PrE to EPI per embryo
+## Re-cast table to wide format and filter for ICM cells only
+spry.ratiocounts <- dcast(spry.lincounts, Experiment + Litter + 
+                                 Embryo_ID + TE_ICM + Cellcount + 
+                                 Stage + Exp_date + Img_date +
+                                 Genotype + Treatment + 
+                                 CH2.marker + ab.2 ~ Identity.lin, 
+                         value.var = 'count')
+spry.ratiocounts <- spry.ratiocounts %>% filter(TE_ICM == 'ICM')
+## Add EPI and DN numbers
+aa <- subset(spry.ratiocounts, DN >= 1)
+aa$DN.EPI <- aa$DN + aa$EPI
+bb <- subset(spry.ratiocounts, is.na(DN) == TRUE)
+bb$DN.EPI <- bb$EPI
+spry.ratiocounts <- rbind(aa, bb)
+rm(aa, bb)
+## Select rows where both PRE and EPI are != 0 
+## (otherwise cannot calculate ratio)
+spry.ratiocounts <- subset(spry.ratiocounts, PRE >= 1 & DN.EPI >= 1)
+## Calculate the ratio as PRE/EPI
+spry.ratiocounts$ratio <- spry.ratiocounts$PRE / spry.ratiocounts$DN.EPI
+
+## Calculate the average number of cells per litter
+avg.litter <- spry %>% group_by(Litter) %>% 
+        summarize(litter.mean = mean(Cellcount))
+## Combine with main table and remove avg table
+spry <- merge(spry, avg.litter)
+rm(avg.litter)
+
