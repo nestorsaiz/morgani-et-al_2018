cdcd43aa085c90b5b8e7c86af7350ccf1156998c
nestorsaiz <nestorsaiz@icloud.com>
2017-12-09 13:45:06 -0500

    updated to process NS and VG data separately

--- identify_spry.R
+++ identify_spry.R
@@ -24,12 +24,17 @@ spread.icm <- function(dataset) {
 ## Using the full range from 32-150 cells generates a continuous cloud
 ## where single clusters cannot be found
 
-bb <- spry %>% filter(Treatment != 'neg.control', 
+set.seed(20170401)
+
+## Perform for Nestor's subset of data
+sns <- subset(spry, Experimenter == 'NS')
+bb <- sns %>% filter(Treatment != 'neg.control', 
                       Genotype2 == 'wt', 
                       ## Exclude litters not stained for GATA6 and NANOG
                       !Litter %in% c('AG', 'V'), 
                       ## Do not subset for wild type embryos
-                      ## as data shows a similar distribution for wt and hets
+                      ## as data shows a similar distribution 
+                      ## for wt and hets
                       ## more data will yield denser clusters
                       #Genotype ==  'wt', 
                       Stage %in% c('32_64', '90_120', '120_150'),  
@@ -38,7 +43,55 @@ bb <- spry %>% filter(Treatment != 'neg.control',
 ## Scatter plot of selected data
 spread.icm(bb)
 
-set.seed(20170401)
+## Calculate three centers for bb (DP, PrE and EPI)
+pp <- kmeans(bb[, 2:3], 3)
+## Extract the minimum center values for each channel 
+## and append to the centers list as the DN center
+dn <- c(pp$centers[, 1][which.min(pp$centers[, 1])], 
+        pp$centers[, 2][which.min(pp$centers[, 2])])
+centers.ns <- rbind(pp$centers, dn, deparse.level = 0)
+
+## Create conditions to test for ICM (is.icm) and morulas (is.morula)
+is.icm <- sns$Cellcount > 31 & sns$TE_ICM == 'ICM' & 
+        !sns$Litter %in% c('V', 'AG')
+is.te <- sns$Cellcount > 31 & sns$TE_ICM == 'TE'
+is.morula <- sns$Cellcount <= 31
+
+## Make matrix to hold sum of squares (rows = icm rows, 4 columns)
+ssq <- matrix(0, length(sns$TE_ICM[is.icm]), 4)
+## and populate with min sum of squares for each cell
+## (min distance to each of the centers for each cell's GATA6 vs NANOG value)
+for(i in 1:4) {
+        ssq[,i] <- (sns$red.ebLogCor[is.icm] - centers.ns[i,1])^2 + 
+                (sns$farred.ebLogCor[is.icm] - centers.ns[i,2])^2
+}
+## calculate what center each cell is closest to 
+## (which sum of squares is smallest)
+min.ssq <- apply(ssq, 1, which.min)
+
+## Create new variable in sns to hold the lineage identity
+## assigned using k-means clustering (Identity.km)
+sns$Identity.km <- rep(NA, nrow(sns))
+## Morulas and TE cells remain unchaged
+sns$Identity.km[is.morula] <- 'morula'
+sns$Identity.km[is.te] <- 'TE'
+## Assign identity to ICM cells based on min.ssq values
+sns$Identity.km[is.icm] <- c('DP', 'EPI', 'PRE', 'DN')[min.ssq]
+
+## Perform for Vidur's subset of data
+svg <- subset(spry, Experimenter == 'VG')
+bb <- svg %>% filter(Treatment != 'neg.control', 
+                     Genotype2 == 'wt', 
+                     ## Do not subset for wild type embryos
+                     ## as data shows a similar distribution 
+                     ## for wt and hets
+                     ## more data will yield denser clusters
+                     #Genotype ==  'wt', 
+                     Stage %in% c('32_64', '90_120', '120_150'),  
+                     TE_ICM %in% c('ICM', 'in')) %>% 
+        select(Cellcount, red.ebLogCor, farred.ebLogCor)
+## Scatter plot of selected data
+spread.icm(bb)
 
 ## Calculate three centers for bb (DP, PrE and EPI)
 pp <- kmeans(bb[, 2:3], 3)
@@ -46,34 +99,38 @@ pp <- kmeans(bb[, 2:3], 3)
 ## and append to the centers list as the DN center
 dn <- c(pp$centers[, 1][which.min(pp$centers[, 1])], 
         pp$centers[, 2][which.min(pp$centers[, 2])])
-centers <- rbind(pp$centers, dn, deparse.level = 0)
+centers.vg <- rbind(pp$centers, dn, deparse.level = 0)
 
 ## Create conditions to test for ICM (is.icm) and morulas (is.morula)
-is.icm <- spry$Cellcount > 31 & spry$TE_ICM == 'ICM' & 
-        !spry$Litter %in% c('V', 'AG')
-is.te <- spry$Cellcount > 31 & spry$TE_ICM == 'TE'
-is.morula <- spry$Cellcount <= 31
+is.icm <- svg$Cellcount > 31 & svg$TE_ICM == 'ICM'
+is.te <- svg$Cellcount > 31 & svg$TE_ICM == 'TE'
+is.morula <- svg$Cellcount <= 31
 
 ## Make matrix to hold sum of squares (rows = icm rows, 4 columns)
-ssq <- matrix(0, length(spry$TE_ICM[is.icm]), 4)
+ssq <- matrix(0, length(svg$TE_ICM[is.icm]), 4)
 ## and populate with min sum of squares for each cell
 ## (min distance to each of the centers for each cell's GATA6 vs NANOG value)
 for(i in 1:4) {
-        ssq[,i] <- (spry$red.ebLogCor[is.icm] - centers[i,1])^2 + 
-                (spry$farred.ebLogCor[is.icm] - centers[i,2])^2
+        ssq[,i] <- (svg$red.ebLogCor[is.icm] - centers.vg[i,1])^2 + 
+                (svg$farred.ebLogCor[is.icm] - centers.vg[i,2])^2
 }
 ## calculate what center each cell is closest to 
 ## (which sum of squares is smallest)
 min.ssq <- apply(ssq, 1, which.min)
 
-## Create new variable in spry to hold the lineage identity
+## Create new variable in svg to hold the lineage identity
 ## assigned using k-means clustering (Identity.km)
-spry$Identity.km <- rep(NA, nrow(spry))
+svg$Identity.km <- rep(NA, nrow(svg))
 ## Morulas and TE cells remain unchaged
-spry$Identity.km[is.morula] <- 'morula'
-spry$Identity.km[is.te] <- 'TE'
+svg$Identity.km[is.morula] <- 'morula'
+svg$Identity.km[is.te] <- 'TE'
 ## Assign identity to ICM cells based on min.ssq values
-spry$Identity.km[is.icm] <- c('DP', 'EPI', 'PRE', 'DN')[min.ssq]
+svg$Identity.km[is.icm] <- c('DP', 'PRE', 'EPI', 'DN')[min.ssq]
+
+## Combine both subsets into one again
+spry <- rbind(sns, svg)
+
+## Order factors as desired
 spry$Identity.km <- factor(spry$Identity.km, levels = c('TE', 'PRE', 'DP', 
                                                         'EPI', 'DN', 'morula'))
 
